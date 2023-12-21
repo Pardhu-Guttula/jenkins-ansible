79037e5b9c8e6369ce97c305e535e8abe5ddcea1
Pardhu Guttula <pardhu_guttula@thbs.india.com>
2023-12-21 12:18:17 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,13 @@
 CONTAINER_ID=$(docker ps -lq)
 echo $CONTAINER_ID
 
-if [ -n "$CONTAINER_ID" ] 
+if [ -n "$CONTAINER_ID" ]
 then
     # Container is running, stop it
     echo "Stopping the running container..."
     docker stop ${CONTAINER_ID}
     echo "Container stopped."
 fi
+
+docker pull pardhuguttula/ansible:$DOCKER_TAG
+docker run -d --name ansible -p 8088:80 pardhuguttula/ansible:$DOCKER_TAG
