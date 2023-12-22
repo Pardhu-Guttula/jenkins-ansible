987cad6f46092b7f2ac704ee36c11c0eb51798d9
Pardhu-Guttula <drpardhu007@gmail.com>
2023-12-22 12:46:08 +0530

    new1

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
+# Get the ID of the most recently created container
 CONTAINER_ID=$(docker ps -lq)
 echo $CONTAINER_ID
-
 if [ -n "$CONTAINER_ID" ]
 then
     # Container is running, stop it
@@ -8,9 +9,17 @@ then
     docker stop ${CONTAINER_ID}
     echo "Container stopped."
 fi
-
-
-# install jq
+#!/bin/bash
+# Get the ID of the most recently created container
+CONTAINER_ID=$(docker ps -lq)
+echo $CONTAINER_ID
+if [ -n "$CONTAINER_ID" ]
+then
+    # Container is running, stop it
+    echo "Stopping the running container..."
+    docker stop ${CONTAINER_ID}
+    echo "Container stopped."
+fi
 # Define the Docker image name
 DOCKER_IMAGE="pardhuguttula/ansible"
 DOCKER_TAG=$(curl -s "https://hub.docker.com/v2/repositories/${DOCKER_IMAGE}/tags/" | jq -r '.results[0].name')
@@ -21,11 +30,8 @@ DOCKER_TAG_NAME=$(echo "$DOCKER_TAG" | tr -cd '[:alnum:]._-' | tr -s '-' | tr '[
 
 # Combine Docker image name and tag to create the container name
 CONTAINER_NAME="${DOCKER_IMAGE_NAME}-${DOCKER_TAG_NAME}"
-
 # Pull the image with the dynamically determined tag
 docker pull "${DOCKER_IMAGE}:${DOCKER_TAG}"
-
 # Run the container
 docker run -d --name "${CONTAINER_NAME}" -p 8088:80 "${DOCKER_IMAGE}:${DOCKER_TAG}"
-
 echo "New container started with Docker tag: ${DOCKER_TAG}"
