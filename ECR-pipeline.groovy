891fdd3cffadb75d9d93b8cb05db0502a05f0d6f
Pardhu-Guttula <drpardhu007@gmail.com>
2023-12-28 17:18:17 +0530

    checking ansible

--- ECR-pipeline.groovy
+++ ECR-pipeline.groovy
@@ -28,7 +28,8 @@ pipeline {
         stage('Build docker image') {
             steps {  
                 script {
-                    dockerImage = docker.build "${IMAGE_REPO_NAME}:${IMAGE_TAG}" 
+                    echo "$IMAGE_TAG"
+                    sh 'docker build -t ${REPOSITORY_URI}:${IMAGE_TAG} .'
                 }
             }
         }
@@ -36,15 +37,15 @@ pipeline {
         stage('pushing to ECR') {
             steps{
                 script {
-                    sh "docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${REPOSITORY_URI}:${IMAGE_TAG}"
-                    sh "docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}"
+                
+                    sh "docker push ${REPOSITORY_URI}:${IMAGE_TAG}"
                 }
             }
         }
 
         stage('Ansible') {
             steps{
-                ansiblePlaybook credentialsId: 'target-server', disableHostKeyChecking: true, extras: "-e DOCKER_TAG=${DOCKER_TAG}", installation: 'ansible', inventory: 'dev.inv', playbook: 'deployment.yml', vaultTmpPath: ''
+                ansiblePlaybook credentialsId: 'target-server', disableHostKeyChecking: true, extras: '-e IMAGE_TAG=${IMAGE_TAG}', installation: 'ansible', inventory: 'dev.inv', playbook: 'deployment.yml', vaultTmpPath: ''
             }
         }
 
